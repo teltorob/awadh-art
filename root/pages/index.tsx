111b110057377d7d4803bf9fc1fe686314960014
Ayush Tiwari <teltorob@gmail.com>
2022-01-04 23:52:04 +0530

    Started homepage, tested JIT

---
+++ root/pages/index.tsx
@@ -0,0 +1,14 @@
+import Head from 'next/head';
+
+export default function Home() {
+  return (
+    <>
+    <section className="herobanner">
+      <div className="flex bg-[#EAE9E5] h-screen w-full px-[8rem] py-8">
+        <h1 className="text-8xl text-gray-800 text-left ">Art of <br /> Awadh and <br /> something</h1>
+        
+      </div>
+    </section> 
+    </>
+  )
+}
