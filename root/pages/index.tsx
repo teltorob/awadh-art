8cf0cde519092efffc8027da9b6a0720ebf53504
Ayush Tiwari <teltorob@gmail.com>
2022-01-06 22:17:53 +0530

    Some placeholder front page

--- root/pages/index.tsx
+++ root/pages/index.tsx
@@ -13,9 +13,18 @@ export default function Home() {
     <>
       <section className="herobanner">
         <div className="flex bg-[#EAE9E5] h-screen w-full pl-[12rem] py-8 z-10">
-          <h1 className="text-8xl text-gray-800 text-left z-10">
-            Art of <br /> Awadh and <br /> something
-          </h1>
+          <div className=" flex flex-col justify-around p-4 w-1/2 z-10">
+            <h3 className=" text-xl ">An Art Store</h3>
+            <h1 className="text-7xl text-gray-800 text-left z-10">
+              Art of <br /> Awadh and <br /> something
+            </h1>
+            <a
+              href="#"
+              className=" p-4 w-3/4 bg-orange-200 text-center text-2xl"
+            >
+              Explore more
+            </a>
+          </div>
           {/* <Image
             // loader={myLoader}
             src="/images/banner-artwork.png"
@@ -23,8 +32,8 @@ export default function Home() {
             width={500}
             height={500}
           /> */}
-          <div className="w-full h-1/2 bg-slate-400 -ml-[5rem] z-0 ">
-            <Image src={banner} alt="" />
+          <div className="w-full flex justify-center bg-slate-400 -ml-[16rem] z-0 ">
+            <Image src={banner} alt="" width={1520} height={780} />
           </div>
         </div>
       </section>
